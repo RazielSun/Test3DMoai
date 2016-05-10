ed6277a58841f83e5036acfe0c50d7c212d29db2
Aleksandr Kardakov <razielsun@gmail.com>
2016-05-10 17:51:55 +0300

    add curve

--- lua/src/tests/test9.lua
+++ lua/src/tests/test9.lua
@@ -1,6 +1,16 @@
 
 local vsh = [=[
-uniform mat4 transform;
+uniform float viewWidth;
+uniform float viewHeight;
+
+uniform mat4 world;
+uniform mat4 worldInverse;
+
+uniform mat4 worldView;
+uniform mat4 worldViewInverse;
+
+uniform mat4 viewProj;
+uniform mat4 worldViewProj;
 
 attribute vec4 position;
 attribute vec2 uv;
@@ -9,16 +19,24 @@ attribute vec4 color;
 varying vec4 colorVarying;
 varying vec2 uvVarying;
 
+const float HORIZON = 0.0;
+const float SPREAD = 0.0;
+const float ATTENUATE = 0.0006;
+
 void main () {
-	vec4 pos = position * transform;
-	if (pos.z > 3000.0)
-	{
-		pos.x = pos.x * 0.6;
-		pos.y -= 500.0;
-	}
+	vec4 pos = position * worldViewProj;
+
     gl_Position = pos;
 	uvVarying = uv;
-    colorVarying = color;
+
+	vec4 t = position * world;
+	float dist = max(0.0, abs(HORIZON - t.z) - SPREAD);
+	t.y -= dist * dist * ATTENUATE;
+	// t.xyz = vec4(t * worldInverse).xyz;
+
+	gl_Position = t * viewProj;
+
+	colorVarying = color;
 }
 ]=]
 
@@ -33,27 +51,31 @@ void main() {
 }
 ]=]
 
-local sizeW, sizeH, sizeD = 6, 1, 10
-
 local M_ = {}
 
 function M_:setup( layer )
 	self:createShaderProgram()
 	self:createShader()
 
-	local prop2 = self:addMyMesh()
+	local prop2 = self:loadPlane()
 	prop2:setLoc( 0, 0, 0 )
 	layer:insertProp ( prop2 )
 
 	local prop = self:loadMesh()
-	prop:setLoc( 0, 0, -200 )
+	prop:setLoc( 0, 128, -200 )
 	layer:insertProp ( prop )
 
 	local camera = MOAICamera.new ()
-	camera:setLoc( 0, 3000, 1000 )
-	camera:lookAt( 0, 0, 500 )
+	-- camera:setLoc( 0, 1500, 3000 )
+	-- camera:lookAt( 0, 0, 500 )
 	camera:setOrtho( false )
+	camera:setLoc( 0, 1600, 500 )
+	camera:setRot( -90, 0, 0 )
+	-- camera:lookAt( 0, 0, 0 )
+	
 	layer:setCamera ( camera )
+
+	camera:seekLoc( 0, 1600, -1000, 10.0 )
 end
 
 function M_:createShaderProgram()
@@ -63,11 +85,25 @@ function M_:createShaderProgram()
 	program:setVertexAttribute ( 2, 'uv' )
 	program:setVertexAttribute ( 3, 'color' )
 
-	program:reserveUniforms ( 1 )
-	program:declareUniform ( 1, 'transform', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
-
-	program:reserveGlobals ( 1 )
-	program:setGlobal ( 1, 1, MOAIShaderProgram.GLOBAL_WORLD_VIEW_PROJ ) -- GLOBAL_WORLD_VIEW_PROJ
+	program:reserveUniforms ( 8 )
+	program:declareUniform ( 1, 'viewProj', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
+	program:declareUniform ( 2, 'viewWidth', MOAIShaderProgram.UNIFORM_FLOAT )
+	program:declareUniform ( 3, 'viewHeight', MOAIShaderProgram.UNIFORM_FLOAT )
+	program:declareUniform ( 4, 'worldInverse', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
+	program:declareUniform ( 5, 'worldView', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
+	program:declareUniform ( 6, 'worldViewInverse', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
+	program:declareUniform ( 7, 'worldViewProj', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
+	program:declareUniform ( 8, 'world', MOAIShaderProgram.UNIFORM_MATRIX_F4 )
+	
+	program:reserveGlobals ( 8 )
+	program:setGlobal ( 1, 1, MOAIShaderProgram.GLOBAL_VIEW_PROJ )
+	program:setGlobal ( 2, 2, MOAIShaderProgram.GLOBAL_VIEW_WIDTH )
+	program:setGlobal ( 3, 3, MOAIShaderProgram.GLOBAL_VIEW_HEIGHT )
+	program:setGlobal ( 4, 4, MOAIShaderProgram.GLOBAL_WORLD_INVERSE )
+	program:setGlobal ( 5, 5, MOAIShaderProgram.GLOBAL_WORLD_VIEW )
+	program:setGlobal ( 6, 6, MOAIShaderProgram.GLOBAL_WORLD_VIEW_INVERSE )
+	program:setGlobal ( 7, 7, MOAIShaderProgram.GLOBAL_WORLD_VIEW_PROJ )
+	program:setGlobal ( 8, 8, MOAIShaderProgram.GLOBAL_WORLD )
 
 	program:load ( vsh, fsh )
 
@@ -96,100 +132,20 @@ function M_:loadMesh()
 	return prop
 end
 
-function M_:addMyMesh()
-	-- default
-	local prop = MOAIProp.new()
-	local mesh = self:createMesh()
+function M_:loadPlane()
+	local file = MOAIFileSystem.loadFile( 'assets/3ds/Plane.mesh' )
+    local mesh = assert( loadstring(file) )()
 
-	prop:setDeck( mesh )	
+    print('mesh', mesh, mesh.textureName)
+
+    mesh:setTexture ( "assets/3ds/moai.png" )
+    mesh:setShader ( self.shader )
+
+    local prop = MOAIProp.new ()
+	prop:setDeck ( mesh )
 	prop:setCullMode ( MOAIGraphicsProp.CULL_BACK )
 	
 	return prop
-end
-
-function M_:createMesh()
-	local vertexFormat = MOAIVertexFormat.new()
-	vertexFormat:declareCoord( 1, MOAIVertexFormat.GL_FLOAT, 3 )
-	vertexFormat:declareUV( 2, MOAIVertexFormat.GL_FLOAT, 2 )
-	vertexFormat:declareColor( 3, MOAIVertexFormat.GL_UNSIGNED_BYTE )
-
-	local vbo = MOAIVertexBuffer.new ()
-	vbo:reserve ( 36 * vertexFormat:getVertexSize ())
-
-	local size = 256
-	local vertexes = {}
-
-	local addControlPoint = function( x, y, z )
-		table.insert( vertexes, { x*size, y*size, z*size } )
-	end
-
-	addControlPoint( 0.5*sizeW, 0.5*sizeH, 0.5*sizeD ) -- 0 +
-	addControlPoint( 0.5*sizeW, 0.5*sizeH, -0.5*sizeD ) -- 1 +
-	addControlPoint( 0.5*sizeW, -0.5*sizeH, 0.5*sizeD ) -- 2 +
-	addControlPoint( 0.5*sizeW, -0.5*sizeH, -0.5*sizeD ) -- 3 +
-	addControlPoint( -0.5*sizeW, 0.5*sizeH, -0.5*sizeD ) -- 4 +
-	addControlPoint( -0.5*sizeW, 0.5*sizeH, 0.5*sizeD ) -- 5 +
-	addControlPoint( -0.5*sizeW, -0.5*sizeH, -0.5*sizeD ) -- 6 +
-	addControlPoint( -0.5*sizeW, -0.5*sizeH, 0.5*sizeD ) -- 7 +
-
-	local normals = {}
-	local addNormal = function( x, y, z )
-		table.insert(normals, {x,y,z})
-	end
-
-	addNormal( 1, 0, 0 )
-	addNormal( 0, 1, 0 )
-	addNormal( -1, 0, 0 )
-	addNormal( 0, -1, 0 )
-	addNormal( 0, 0, -1 )
-	addNormal( 0, 0, 1 )
-
-	local uvs = {}
-	local addUVs = function( u, v )
-		table.insert( uvs, {u,v} )
-	end
-
-	addUVs(0,0)
-	addUVs(0,1)
-	addUVs(1,1)
-	addUVs(1,0)
-
-	local color = {1,1,1}
-
-	local setVertex = function( p, uv, n )
-		vbo:writeFloat ( unpack(vertexes[p+1]) ) 
-		-- vbo:writeFloat( unpack(normals[n]) )
-		vbo:writeFloat( unpack(uvs[uv]) )
-		vbo:writeColor32 ( unpack(color) )
-	end
-
-	local setTriangle = function( p1, p2, p3, uv1, uv2, uv3, n )
-		setVertex( p1, uv1, n )
-		setVertex( p2, uv2, n )
-		setVertex( p3, uv3, n )
-	end
-
-	local setPoly = function( p1, p2, p3, p4, uv1, uv2, uv3, uv4, n )
-		setTriangle( p1, p2, p3, uv1, uv2, uv3, n )
-		setTriangle( p3, p4, p1, uv3, uv4, uv1, n )
-	end
-
-	setPoly( 0, 2, 3, 1, 1, 2, 3, 4, 1 ) 
-	setPoly( 4, 6, 7, 5, 1, 2, 3, 4, 1 )
-	setPoly( 4, 5, 0, 1, 1, 2, 3, 4, 1 )
-	setPoly( 7, 6, 3, 2, 1, 2, 3, 4, 1 )
-	setPoly( 5, 7, 2, 0, 1, 2, 3, 4, 1 )
-	setPoly( 1, 3, 6, 4, 1, 2, 3, 4, 1 )
-	-- MESH
-	local mesh = MOAIMesh.new ()
-	mesh:setVertexBuffer( vbo, vertexFormat )
-	mesh:setTexture ( "assets/3ds/moai.png" )
-	mesh:setPrimType ( MOAIMesh.GL_TRIANGLES )
-	mesh:setShader ( self.shader )--MOAIShaderMgr.getShader( MOAIShaderMgr.MESH_SHADER ) )
-	-- mesh:setShader ( MOAIShaderMgr.getShader ( MOAIShaderMgr.LINE_SHADER_3D ))
-	mesh:setTotalElements( vbo:countElements( vertexFormat ) )
-	mesh:setBounds( vbo:computeBounds( vertexFormat ) )
-	return mesh
 end
 
 return M_
